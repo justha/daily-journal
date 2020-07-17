8b4e96a91918f6ceb62688e64183a31d567e7fc1
justha <nganha42@gmail.com>
2020-07-16 20:35:50 -0500

    rendered collections of journal entries

---
+++ scripts/journalEntry.js
@@ -0,0 +1,15 @@
+/*
+ *  Purpose: To render a single journal entry as an
+ *           HTML representation of the data
+ * 
+ *  *  Display the entry's full text, and the date it was entered below.
+ * 
+ */
+export const JournalEntryComponent = (entry) => {
+    return `
+        <section id="entry--${entry.id}" class="journalEntry">
+            <div class="">${entry.date}</div>
+            <div class="">${entry.entry}</div>
+        </section>
+    `
+}
